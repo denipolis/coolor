f9bd14b798e7fd5c0928bf6fba42831c93570c54
Denis Polishchuk <denipol07@gmail.com>
2023-03-26 10:11:02 +0300

    SEO Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import './globals.css';
 export const metadata = {
   title: 'Coolor',
   description: 'Simple and minimalistic color picker. Made with love 💘',
+  openGraph: {
+    images: [
+      "https://coolor.vercel.app/logo.png"
+    ]
+  }
 };
 
 export default function RootLayout({
